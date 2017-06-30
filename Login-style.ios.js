359b0f5fcc34886a3f9721b19f9ebbad83e10de1
Claudia Lisboa <ccalumby@gmail.com>
2017-06-30 10:12:37 -0400

    Login-style

---
+++ Login-style.ios.js
@@ -0,0 +1,64 @@
+'use strict';
+
+
+import { StyleSheet } from 'react-native'
+
+var Login = require('./Login');
+
+
+export default StyleSheet.create({
+  container: {
+   flex:1,
+        flexDirection: 'column',
+        backgroundColor:'#f5fcff',
+        justifyContent: 'center',
+        alignItems:'center',
+        width: null,
+        height: null,
+    },
+
+    backgroundImage:{
+         flex:1,
+         resizeMode:'cover', //or 'stretch'
+    },
+    loggreen:{
+        //flex:1,
+        resizeMode:'center', // 'center' or 'contain'
+        alignSelf:'center',
+        // height:250,
+        // width:250,
+        //borderWidth:2,
+    },
+
+    buttonContainer:{
+        flex:1,
+        flexDirection: 'column',
+        backgroundColor:'rgba(0,0,0,0)',
+        justifyContent: 'flex-end',
+        alignItems:'center',
+        width: null,
+        height: null,
+    },
+
+    buttonGmail:{
+        //flex:1,
+        resizeMode:'center', // 'center' or 'contain'
+        // alignSelf:'center',
+        // flexDirection:'column',
+        // justifyContent: 'flex-end',
+        // height:250,
+        // width:250,
+        //borderWidth:2,
+    },
+    buttonSlack:{
+        //flex:1,
+        resizeMode:'center', // 'center' or 'contain'
+        // alignSelf:'center',
+        // flexDirection:'column', 
+        // justifyContent: 'flex-end',
+        // height:250,
+        // width:250,
+        //borderWidth:2,
+    }
+})
+
--- Login.js
+++ Login.js
@@ -14,6 +14,8 @@ import {
   
 } from 'react-native';
 
+import styles from './Login-style' // Imports platform-specific style
+
 // I don't know how to use ES6
 /*class Login extends Component{ 
     constructor(props){
@@ -26,21 +28,15 @@ import {
 var Login = React.createClass({ 
     render: function () {
         return(
-           
             <Image source={require('./img/Background_LesPitonneux.jpg')} style={styles.container}> 
-                
                 <Image style={styles.loggreen}
                 source={require('./img/Logo_vertPitonneux.png')}/>
-                <TouchableHighlight
-                   onPress={() => {this.props.onPress();}}>
-                
                     <View style={styles.buttonContainer}>
                         <Image style={styles.buttonGmail}
                         source={require('./img/buttonLoginGmail.png')}/>
                         <Image style={styles.buttonSlack}
                         source={require('./img/buttonLoginSlack.png')}/>
                     </View>
-                </TouchableHighlight>
             </Image>
             
 /*            <View style={styles.container}> 
@@ -59,57 +55,6 @@ var Login = React.createClass({
     }
 });
 
-var styles = StyleSheet.create({
-    container:{
-        flex:1,
-        flexDirection: 'column',
-        backgroundColor:'#f5fcff',
-        justifyContent: 'center',
-        alignItems:'center',
-        width: null,
-        height: null,
-    },
-    buttonContainer:{
-        flex:1,
-        flexDirection: 'column',
-        backgroundColor:'rgba(0,0,0,0)',
-        justifyContent: 'flex-end',
-        alignItems:'center',
-        width: null,
-        height: null,
-    },
-    backgroundImage:{
-         flex:1,
-         resizeMode:'cover', //or 'stretch'
-    },
-    loggreen:{
-        //flex:1,
-        resizeMode:'center', // 'center' or 'contain'
-        alignSelf:'center',
-        // height:250,
-        // width:250,
-        borderWidth:2,
-    },
-    buttonGmail:{
-        //flex:1,
-        resizeMode:'center', // 'center' or 'contain'
-        // alignSelf:'center',
-        // flexDirection:'column',
-        // justifyContent: 'flex-end',
-        // height:250,
-        // width:250,
-        borderWidth:2,
-    },
-    buttonSlack:{
-        //flex:1,
-        resizeMode:'center', // 'center' or 'contain'
-        // alignSelf:'center',
-        // flexDirection:'column', 
-        // justifyContent: 'flex-end',
-        // height:250,
-        // width:250,
-        borderWidth:2,
-    }
-});
+
 module.exports = Login;
 //export default Login; -- I don't know how to use ES6 --
