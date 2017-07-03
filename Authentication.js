181db22f400e68c68cc7640e669eae927883c7c7
Claudia <ccalumby@gmail.com>
2017-07-03 19:40:24 -0400

    Authentication

--- Authentication.js
+++ Authentication.js
@@ -1,2 +1,19 @@
-import React, {Component} from 'react';
-import {StyleSheet, View, TextInput} from 'react-native';
+import OAuthManager from 'react-native-oauth';
+ 
+const manager = new OAuthManager('firestackexample')
+manager.configure({
+    slack: {
+    client_id: 'YOUR_CLIENT_ID',
+    client_secret: 'YOUR_CLIENT_SECRET'
+  },
+  google: {
+    callback_url: `io.fullstack.FirestackExample:/oauth2redirect`,
+    client_id: 'YOUR_CLIENT_ID',
+    client_secret: 'YOUR_SECRET'
+  }
+});
+ 
+// ... 
+manager.authorize('google', {scopes: 'profile email'})
+.then(resp => console.log('Your users ID'))
+.catch(err => console.log('There was an error'));
