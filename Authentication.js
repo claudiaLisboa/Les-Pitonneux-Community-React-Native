871a171dff925e0c7a2768896743ed2b52723ef6
Claudia <ccalumby@gmail.com>
2017-07-05 01:12:17 -0400

    OAuth Google

--- Authentication.js
+++ Authentication.js
@@ -1,18 +1,20 @@
 import OAuthManager from 'react-native-oauth';
  
-const manager = new OAuthManager('firestackexample')
-manager.configure({
+const config = {
     slack: {
-    client_id: 'YOUR_CLIENT_ID',
-    client_secret: 'YOUR_CLIENT_SECRET'
-  },
-  google: {
-    callback_url: `io.fullstack.FirestackExample:/oauth2redirect`,
-    client_id: 'YOUR_CLIENT_ID',
-    client_secret: 'YOUR_SECRET'
-  }
-});
- 
+        client_id: 'YOUR_CLIENT_ID',
+        client_secret: 'YOUR_CLIENT_SECRET'
+    },
+    google: {
+        callback_url: `com.googleusercontent.apps.385453512743-ffbfi7un3623a2307o6mgil0s2qq8qq2:/google`,
+        client_id: '385453512743-ffbfi7un3623a2307o6mgil0s2qq8qq2.apps.googleusercontent.com',
+    }
+};
+// Creating the manager.
+const manager = new OAuthManager('com.googleusercontent.apps.385453512743-ffbfi7un3623a2307o6mgil0s2qq8qq2')
+// Configuring the manager.
+manager.configure(config);
+
 // ... 
 manager.authorize('google', {scopes: 'profile email'})
 .then(resp => console.log('Your users ID'))
